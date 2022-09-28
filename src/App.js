8fb21e9385cea4b5dff067a86e92489cd6b274fc
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-09-28 21:09:27 +0430

    Update App .js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
-import Rockect from './components/pages/Rockect';
 import Missions from './components/pages/Missions';
 import Profile from './components/pages/Profile';
 
@@ -10,7 +9,6 @@ function App() {
     <div className="App">
       <Nav />
       <Routes>
-        <Route path="/" element={<Rockect />} />
         <Route path="/missions" element={<Missions />} />
         <Route path="/profile" element={<Profile />} />
       </Routes>
