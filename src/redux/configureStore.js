b56e7b6975c3d293f89d9bb0048159822075ea30
usernameBakhtiyor <harhil46@gmail.com>
2022-09-30 02:33:27 +0500

    Import Rockets into configureStore.js

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missions';
+import rockectReducer from './rockets/RockectReducer';
 
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
+    rockects: rockectReducer,
   },
 });
 
