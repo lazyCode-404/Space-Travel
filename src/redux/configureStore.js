bd685ef81e987e3abc46b4c3b8aacd80504f3d41
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-09-29 12:53:39 +0430

    Change the crlf linebrakes to LF

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missions';
 
-
 const store = configureStore({
   reducer: {
     missions: missionsReducer,
