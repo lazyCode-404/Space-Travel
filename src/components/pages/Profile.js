71a711a195f0c8fcea371e78009e8de882e3f74a
zakiyousufi <ahmadzakiyousufi3@gmail.com>
2022-09-29 19:44:45 +0430

    Add Active Missions on Myprofile

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
+import { useSelector } from 'react-redux';
 
-const Profile = () => (
-  <div>My Profile</div>
-);
+const Profile = () => {
+  const { missions } = useSelector((state) => state.missions);
+  const Missions = missions.filter((m) => m.isMember);
+  return (
+    <>
+      <div className="profile">
+        <div className="profile-missions">
+          <h2>My Missions</h2>
+          <ListGroup>
+            {Missions.map(((m) => (
+              <ListGroup.Item key={m.mission}>{m.mission_name}</ListGroup.Item>
+            )))}
+          </ListGroup>
+        </div>
+        <div className="profile-rockets">
+          <h2>My Rockets</h2>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Profile;
