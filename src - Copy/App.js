aedd35c4ced5940108f375570efa6d369409a2d1
usernameBakhtiyor <harhil46@gmail.com>
2022-09-30 20:50:09 +0500

    bnbnbnn

---
+++ src - Copy/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav';
+import Missions from './components/pages/Missions';
+import Dragons from './components/pages/Dragons';
+import Profile from './components/pages/Profile';
+import Rockect from './components/pages/Rockect';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Rockect />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/dragons" element={<Dragons />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
